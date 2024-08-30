27c5ca980ceba07da9a7b6e87463cfb7ea9eac81
JoshCastor36 <jo349212@ucf.edu>
2024-08-30 12:35:30 -0400

    Add files via upload

---
+++ program0_Joshua_Castor.c
@@ -0,0 +1,13 @@
+//********************************************************
+// program0.c
+// Author: Joshua Castor
+// Date: 9/27/23
+// Class: COP 3223C, Professor Parra
+// Purpose: This program prints a specific message to the
+// command line. The main purpose of this assignment is to
+// check and see whether I can connect to the Codespaces Server.
+// Input: None
+//
+// Output: (to the command line) A brief Message crafted by 
+// Dr. Moran to test running a C program on Codespaces
+// //********************************************************
